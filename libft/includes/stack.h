442daf7bfb6d9be181a4e5d570a8f4c9feee18c0
Noelle MOSTERT <nmostert@e4r20p2.wethinkcode.co.za>
2018-08-29 15:59:03 +0200

    working makefile

--- libft/includes/stack.h
+++ libft/includes/stack.h
@@ -6,7 +6,7 @@
 /*   By: nmostert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/24 14:08:33 by nmostert          #+#    #+#             */
-/*   Updated: 2018/08/29 15:26:26 by nmostert         ###   ########.fr       */
+/*   Updated: 2018/08/29 15:50:43 by nmostert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ typedef struct	s_stack
 }				t_stack;
 
 int				isempty(t_stack *s);
-t_stack			*newstack(const void *content, size_t content_size);
+t_stack			*stacknew(const void *content, size_t content_size);
 t_list			*peek(t_stack *s);
 t_list			*stackpop(t_stack *s);
 void			stackpush(t_stack *s, t_list *elem);
--- libft/stack_src/main.c
+++ libft/stack_src/main.c
@@ -6,7 +6,7 @@
 /*   By: nmostert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 11:40:40 by nmostert          #+#    #+#             */
-/*   Updated: 2018/08/29 15:02:37 by nmostert         ###   ########.fr       */
+/*   Updated: 2018/08/29 15:54:10 by nmostert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,18 @@
 int main (void)
 {
 	t_stack *stack;
+	t_list	*tmp;
 
-	stack = newstack("random crap", 50);
+	stack = stacknew("random crap", 50);
 	//stack->head = newstack("more crap", 50);
+	stackpush(stack, ft_lstnew("av", 10));
+	stackpush(stack, ft_lstnew("ab", 10));
+
+	tmp = peek(stack);
+	while (tmp != NULL)
+	{
+		printf("%s\n", (char *)tmp->content);
+		tmp = tmp->next;
+	}
 	printf("%s\n", (char *)peek(stack)->content);
 }
